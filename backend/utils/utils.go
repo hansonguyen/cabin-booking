13f1d151dbb14bb9afc1c16ae83a5c177667750b
Hanson Nguyen <64661526+hansonguyen@users.noreply.github.com>
2023-09-04 22:07:59 -0700

    presignup lambda

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -4,10 +4,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Booking struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	UserName    string	`bson:"userName" json:"userName"`
+	UserName  string	`bson:"userName" json:"userName"`
 	UserId    string	`bson:"userId" json:"userId"`
-	Title 		string	`bson:"title" json:"title"`
-	Start 		string	`bson:"start" json:"start"`
-	End   		string	`bson:"end" json:"end"`
-	AllDay		bool		`bson:"allDay" json:"allDay"`
+	Title     string	`bson:"title" json:"title"`
+	Start     string	`bson:"start" json:"start"`
+	End       string	`bson:"end" json:"end"`
+	AllDay    bool		`bson:"allDay" json:"allDay"`
 }
